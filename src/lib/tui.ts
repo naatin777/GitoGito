35512064dfb0e10fbe030ca7adaed114c71979ef
naatin777 <76672482+naatin777@users.noreply.github.com>
2026-01-10 17:17:17 +0900

    style: clean up unused variables and comments
    Removed commented-out code and unused variables to improve code readability and maintainability.

--- src/lib/tui.ts
+++ src/lib/tui.ts
@@ -3,14 +3,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../app/store.ts";
 
-const _encoder = new TextEncoder();
-// const write = (txt: string) => Deno.stdout.writeSync(_encoder.encode(txt));
-// const ENTER_ALT_SCREEN = "\x1b[?1049h\x1b[2J\x1b[H";
-// const EXIT_ALT_SCREEN = "\x1b[?1049l";
-
-export async function runTui(component: React.ReactNode, _isAltScreen = true) {
-  // write(_isAltScreen ? ENTER_ALT_SCREEN : "");
-
+export async function runTui(component: React.ReactNode) {
   try {
     const wrappedComponent = React.createElement(
       Provider,
@@ -21,7 +14,5 @@ export async function runTui(component: React.ReactNode, _isAltScreen = true) {
     await instance.waitUntilExit();
   } catch (err) {
     console.error(err);
-  } finally {
-    // write(_isAltScreen ? EXIT_ALT_SCREEN : "");
   }
 }
